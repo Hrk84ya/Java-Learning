7ac3283bf8a8ad8f299554887c8a24b0ec17706d
Harsh Kumar <hrk84ya@gmail.com>
2025-11-24 23:15:55 +0530

    MultiVariable File

---
+++ 01-basics/MultiVariable.java
@@ -0,0 +1,16 @@
+// Multiple Variables in JAVA
+public class MultiVariable {
+    public static void main(String[] args){
+        int a=10;
+        int b=20, c=30, d=40;
+        int e,f,g,h;
+        e=f=g=h=50;
+
+        System.out.println(a+b);
+        System.out.println(c*d);
+        System.out.println(e+f);
+        System.out.println(g);
+        System.out.println(h);
+
+    }
+}
