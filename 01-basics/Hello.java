76c846fc46eff4997f4a8c12c7736bed5a3e1321
Harsh Kumar <hrk84ya@gmail.com>
2025-11-24 23:15:37 +0530

    Hello World!

---
+++ 01-basics/Hello.java
@@ -0,0 +1,24 @@
+public class Hello{
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
+
+/* 
+Types of variables in Java
+==========================
+1. Local variables
+2. Instance variables
+3. Static variables
+
+
+Types of Methods
+================
+1. Static methods: Called without creating an object
+2. Instance methods: Called by creating an object
+3. With Parameters and return Type 
+4. Without Parameters
+
+
+*/
+
